5bb5964c0ca5543935d87b35fbb70cd09136dc61
chonqk123 <91725926+chonqk123@users.noreply.github.com>
2023-07-13 12:16:19 +0700

    Django Tutorial Part 7: Sessions framework
    ready

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
 
@@ -15,14 +15,30 @@ def index(request):
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
     
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    
+class AuthorDetailView(generic.DetailView):
+    model = Author
