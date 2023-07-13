8b57ea450692abdaf85c689c69da8c50196603e0
chonqk123 <91725926+chonqk123@users.noreply.github.com>
2023-07-13 14:08:25 +0700

    Django Tutorial Part 8: User authentication and permissions

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,10 @@
+import uuid
+from datetime import date
 from django.db import models
 from django.urls import reverse
-import uuid
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
 class Genre(models.Model):
@@ -39,6 +43,9 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    permissions = (("can_mark_returned", "Set book as returned"),)
+
     
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -54,7 +61,11 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
-        
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
 class Author(models.Model):
     """Model representing an author."""
     first_name = models.CharField(max_length=100)
